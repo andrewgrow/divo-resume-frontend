87d3442c8671981a0eccc891c66548ad39c7509c
Andrew Gahov <andrew.gahov@gmail.com>
2025-07-26 19:21:28 +0200

    change background color and css styles;

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,19 @@
 // /src/App.jsx
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="bg-white/80 rounded-lg shadow-2xl p-8 max-w-2xl mt-20 mb-20 mx-1 ">
+                <h2 className="text-2xl font-bold mb-4 text-indigo-900">Content card</h2>
+                <p className="mb-2">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis mattis lectus. Vivamus luctus, turpis et fermentum aliquet, libero ex mattis nisi, in sodales dui ipsum ac metus. Fusce lectus velit, efficitur ac scelerisque ac, elementum sed orci. Suspendisse ligula nisl, bibendum id lectus in, luctus fermentum velit. Sed in elit eu erat efficitur hendrerit eget a tortor. Etiam hendrerit suscipit ligula, at bibendum massa luctus quis. Proin commodo tincidunt lorem et venenatis.
+                </p>
+                <p className="mb-2">
+                    Ut id tellus dui. Vivamus dictum orci at nibh aliquet, et efficitur velit posuere. Praesent malesuada faucibus orci, pulvinar ornare lacus laoreet sit amet. Vestibulum eu neque mi. Nunc non turpis id metus ultricies accumsan. Aliquam nec semper tortor. Phasellus quis elit ut lorem ornare tempor. Nullam nisl sapien, ornare et sem sit amet, aliquet dictum arcu. Praesent sem arcu, ornare euismod ante non, congue condimentum ex.
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default App
